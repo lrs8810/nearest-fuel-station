efa31cd28b5da6e601290dad99e873add5cfb99f
Laura Schulz <lrs8810@gmail.com>
2019-12-20 11:06:44 -0700

    Refactor facade to create station poro

--- app/facades/charging_station_results.rb
+++ app/facades/charging_station_results.rb
@@ -1,18 +1,19 @@
 class ChargingStationResults
+  attr_reader :station
+
   def initialize(location)
     @nrel_service = NrelService.new(location)
+    get_station_data
+    @station = Station.new(get_station_data)
   end
 
-  def get_station
+  def get_station_data
     station_data = @nrel_service.get_json
-    station_hash = {
+    {
       name: station_data['station_name'],
       address: "#{station_data['street_address']}, #{station_data['city']}, #{station_data['state']} #{station_data['zip']}",
       fuel_type: station_data['fuel_type_code'],
       access_times: station_data['access_days_time']
     }
-    binding.pry
-    Station.new(station_hash)
   end
-
 end
