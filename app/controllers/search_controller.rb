ee26e0fa2797f91992eb86eb0b31733dab2fbdb5
Laura Schulz <lrs8810@gmail.com>
2019-12-20 10:42:05 -0700

    Refactor controller action to service and facade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,7 @@
 class SearchController < ApplicationController
   def index
-    connection = Faraday.new('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json') do |faraday|
-      faraday.params['api_key'] = ENV['NREL_API_KEY']
-      faraday.params['fuel_type'] = 'ELEC'
-      faraday.params['location'] = '1331 17th St LL100, Denver, CO 80202'
-      faraday.adapter Faraday.default_adapter
-    end
-    response = connection.get
-    binding.pry
-
-    raw_data = JSON.parse(response.body)['fuel_stations'].first
+    @results = ChargingStationResults.new(params['location'])
+    @results.get_station
     binding.pry
   end
 end
