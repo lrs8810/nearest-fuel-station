2223ff0d5678f56f37409f4d5092d0caaf6b9717
Laura Schulz <lrs8810@gmail.com>
2019-12-20 10:03:54 -0700

    Add NREL api connection, response, and data parsing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,15 @@
 class SearchController < ApplicationController
   def index
-  end 
+    connection = Faraday.new('https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json') do |faraday|
+      faraday.params['api_key'] = ENV['NREL_API_KEY']
+      faraday.params['fuel_type'] = 'ELEC'
+      faraday.params['location'] = '1331 17th St LL100, Denver, CO 80202'
+      faraday.adapter Faraday.default_adapter
+    end
+    response = connection.get
+    binding.pry
+
+    raw_data = JSON.parse(response.body)['fuel_stations'].first
+    binding.pry
+  end
 end
